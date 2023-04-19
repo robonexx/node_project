78fbfcba8d42bdfec6229c2952ea7db242fb13cf
user.name "robonexx <user.email "robertwagar@gmail.com>
2023-04-19 17:08:36 +0200

    word seach function working

--- readFiles.js
+++ readFiles.js
@@ -19,10 +19,10 @@ const isMatch = (search, content) => {
 
   // Match any word in the content.
   if (content.match(new RegExp(search, 'g'))) {
-    console.log(`the word ${search} exists in the file`);
+    /* console.log(`the word ${search} exists in the file`); */
     return true;
   }
-  console.log(`Could not find the word ${search} in this file`);
+  /* console.log(`Could not find the word ${search} in this file`); */
   return false;
 };
 
@@ -31,16 +31,26 @@ function readCurrentFile(filePath) {
     const data = fs.readFileSync(filePath);
     console.log('from func : \n' + data.toString());
 
-    if (data.toString().includes('digital')) {
+    /* if (data.toString().includes('digital')) {
       console.log('found the word');
-    }
+    } */
     // function isMatch to find specific word in text
-    isMatch('technology', data.toString());
-    isMatch('company', data.toString());
-    isMatch('roberto', data.toString());
+    if (
+      isMatch('html', data.toString()) == true ||
+      isMatch('css', data.toString()) == true ||
+      isMatch('javascript', data.toString()) == true ||
+      isMatch('react', data.toString()) == true ||
+      isMatch('node.js', data.toString()) == true
+    ) {
+      console.log('You should apply for a developer job');
+    }
+    // isMatch('javascript', data.toString());
+    // isMatch('react', data.toString());
+    // isMatch('node.js', data.toString());
+    // isMatch('next.js', data.toString());
   } catch (error) {
     console.error(`Got an error trying to read the file: ${error.message}`);
   }
 }
 
-readCurrentFile('./filesToReadFrom/text.txt');
+readCurrentFile('./newFile/latestInput.txt');
