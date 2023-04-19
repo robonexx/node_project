a6e9ca95d65c12644c4098e2343717fb0d0ef4ba
user.name "robonexx <user.email "robertwagar@gmail.com>
2023-04-19 16:38:58 +0200

    function to find specific word in a file, working

--- readFiles.js
+++ readFiles.js
@@ -6,25 +6,41 @@ console.log(data);
 
 const wordsArr = ['digital', 'technology', 'agile', 'future'];
 
+const isMatch = (search, content) => {
+  // Lowarize the search term and the content.
+  search = search.toLowerCase();
+  content = content.toLowerCase();
+
+  // Remove all inner spaces and keep only one space between two words.
+  search = search.replace(/\s\s+/g, ' ').trim();
+
+  // Add the pipe operator to match any single word.
+  search = search.replace(' ', '|');
+
+  // Match any word in the content.
+  if (content.match(new RegExp(search, 'g'))) {
+    console.log(`the word ${search} exists in the file`);
+    return true;
+  }
+  console.log(`Could not find the word ${search} in this file`);
+  return false;
+};
+
 function readCurrentFile(filePath) {
   try {
     const data = fs.readFileSync(filePath);
     console.log('from func : \n' + data.toString());
+
+    if (data.toString().includes('digital')) {
+      console.log('found the word');
+    }
+    // function isMatch to find specific word in text
+    isMatch('technology', data.toString());
+    isMatch('company', data.toString());
+    isMatch('roberto', data.toString());
   } catch (error) {
     console.error(`Got an error trying to read the file: ${error.message}`);
   }
 }
 
-function findKeyWords(func) {
-    const data = func
-    let isMatch;
-
-    if (data.contains('digital')) {
-        isMatch = true
-    } else {
-        isMatch = false
-    }
-    return isMatch
-}
-
-readCurrentFile('./newFile/latestInput.txt');
+readCurrentFile('./filesToReadFrom/text.txt');
